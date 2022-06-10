3cc5f5a96e4b8443631e139c03fbd0094ed11467
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-10 20:14:10 +0800

    Delete prediction directory

--- prediction/evidence_helper.py
+++
@@ -1,5 +0,0 @@
-# I now that this is not great code style and I apologize but the initial code did not need such an extra variable
-# and saving it in this file was much more convenient for adding the evidence accumulation method.
-
-# probability of the data used on the most recently trained (fitted) RDT
-DATASET_PROBABILITY = 0
